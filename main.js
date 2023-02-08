85eb07f247e557677ab984169e1d6e9e91b2f00d
ryan <ryan.patel21@imperial.ac.uk>
2023-02-08 11:59:51 +0000

    Updated resume path

--- main.js
+++ main.js
@@ -128,4 +128,8 @@ contactNavBtn.addEventListener("click", () => {
     toggleAboutContact(cnt);
 })
 
+var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+if (a.indexOf('html') > -1) { //Check of html String in URL.
+   url = url.substring(0, newURL.lastIndexOf("."));
+}
 
